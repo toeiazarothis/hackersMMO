f72a671d08409023f7e41015e2ea5bdd2ccc5198
Toeiazarothis <patrick.de.almeida89@gmail.com>
2017-03-24 01:37:38 +0100

    correction du tuto delete ip. ( v 0.1.2)

--- modal.php
+++ modal.php
@@ -21,8 +21,8 @@
                     1/ la méthode simple
                     Si vous avez un ordinateur ou une tablette, téléchargez l'émulateur Android nommé Nox, que vous trouverez sur le site bignox. Installez le sur votre pc ou tablette. Il s'agit d'un programme qui va simuler dans votre pc un périphérique Android. Une fois nox installé, lancez le et allez chercher hackers dans Google play, comme sur votre smartphone. Installez le, connectez vous à votre compte et c'est tout!
                     Vous constaterez que votre liste d'ip sera vide dans la version emulée du jeu. C'est normal, le jeu sauvegarde la liste d'ip en local ! Ce qui signifie que vous pouvez avoir plusieurs listes si vous avez plusieurs périphériques Android, comme par exemple, un smartphone et un émulateur !
-                    Ensuite, il ne vous reste plus qu'à vous connecter d'abord sur l'emulateur. Si vous avez le virus, il supprimera la liste qui se trouve dans la version emulée du jeu. Donc vous pourrez aller supprimer le virus de vos fichiers, et ensuite vous connecter sereinement sur votre périphérique habituel, et comme vous aurez supprimé le virus via l'emulateur, votre liste principale sera sauve !
-                    @everyone
+                    Ensuite, il ne vous reste plus qu'à vous connecter d'abord sur l'emulateur. Si vous avez le virus, il supprimera la liste qui se trouve dans la version emulée du jeu. Donc vous pourrez aller supprimer le virus de vos fichiers, et ensuite vous connecter sereinement sur votre périphérique habituel, et comme vous aurez supprimé le virus via l'emulateur, votre liste principale sera sauve !<br>
+                    <hr>
                     2/ la méthode chiante
                     ! Nécessite un smartphone rooté !
                     La liste d'ip est sauvegardée en local sur votre téléphone, dans un dossier dont le nom comprend okitoo. Malheureusement, ce dossier n'est visible et accessible que si votre smartphone est rooté. Donc, assumons que ce soit le cas. Allez simplement dans le dossier de données du jeu, et faites un backup du XML contenant la liste d'ip avant de vous connecter. Si vous avez le virus, supprimez le de votre gateway, puis quittez le jeu, restaurez le backup du XML, et relancez le jeu. Votre liste est revenue !
@@ -39,3 +39,31 @@
       </div>
     </div>
   </div>
+
+  <!-- Portfolio Modal Prochain client -->
+    <div class="portfolio-modal modal fade" id="Modal1" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+              <div class="rl"> </div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 col-lg-offset-2">
+                <div class="modal-body">
+                  <!-- Project Details Go Here -->
+                  <h2></h2>
+                  <p class="item-intro text-muted"><br>
+                  </p>
+                  <img class="img-responsive img-centered" src="" alt="">
+                  <p></p>
+                  <img class="img-responsive img-centered" src="" alt="">
+                  <button type="button" class="btn btn-primary" data-dismiss="modal">Fermer la fenêtre</button> </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
