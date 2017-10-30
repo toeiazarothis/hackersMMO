3b289c84739e97097ebda43510d8f241060db15e
Toeiazarothis <patrick.de.almeida89@gmail.com>
2017-10-30 11:54:35 +0100

    modification du footer

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
     <br>
     <div class="row">
       <div class="col-md-10 col-md-offset-1">
-        <span class="copyright" style="color:white;">Copyright <a href="http://patrick.hol.es" target="_blank">ToeiAzarothis</a> All rights reserved
+        <span class="copyright" style="color:white;">Copyright - <a href="http://patrick.hol.es" target="_blank">ToeiAzarothis</a> All rights reserved
         </span>
       </div>
     </div>
